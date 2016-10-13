8d1571761c552bc80ad988307ca5eace487940d1
Young Ju (Sally) Park <parkyngj@gmail.com>
2016-10-12 21:01:49 -0700

    Change users_games table name to games_users

--- db/migrate/20161012205622_users_games.rb
+++
@@ -1,8 +0,0 @@
-class UsersGames < ActiveRecord::Migration
-  def change
-    create_table :users_games, id: false do |t|
-      t.belongs_to :user, index:true
-      t.belongs_to :game, index:true
-    end
-  end
-end
