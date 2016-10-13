a11e3fcd61155a28ca1b8985bb5946efd9d7ea87
Young Ju Park <parkyngj@gmail.com>
2016-10-13 15:57:14 -0700

    Add funtional basic adapter method for making model object out of API return

--- app/models/gb_api_adapter.rb
+++ app/models/gb_api_adapter.rb
@@ -6,14 +6,17 @@ module GbApiAdapter
   def self.query_for_gameboy(name)
     url = "http://www.giantbomb.com/api/games/?api_key=#{KEY}&filter=platforms:57,name:#{name}&format=json&limit=100"
 
-    p response = HTTParty.get(url)
+    response = HTTParty.get(url)
+
   end
 
   def self.platform_query(name)
     url = "http://www.giantbomb.com/api/platforms/?api_key=#{KEY}&filter=name:#{name}&format=json&limit=100&field_list=abbreviation,api_detail_url,company,deck,id,image,name,original_price,release_date&developers=true"
 
-    p response = HTTParty.get(url)
+    response = HTTParty.get(url)
+    parsed_response = response.parsed_response
+    response_hash = parsed_response["results"][0]
 
-
+    Platform.create(name: response_hash['name'])
   end
 end
